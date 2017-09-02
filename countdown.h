c2f02e84a7489a57b4d9d586bf578ed9522b247f
Liam O'Neill <l.oneill20@nuigalway.ie>
2017-09-02 17:14:32 +0100

    Minor change to order for consistency with countdown.c

--- countdown.h
+++ countdown.h
@@ -3,11 +3,11 @@
 
 void enterLetters(char *letters);
 void allLowerCase(char *string);
-void countLetters(char *letters, int *letterCount);
 int validUserWord(char *word, char *letters);
+void countLetters(char *letters, int *letterCount);
 
-int wordTest(char *word, char *userWord, char *letterChoice);
 void dictionarySearch(char *letters, char *yourWord, char *compuWord);
+int wordTest(char *word, char *userWord, char *letterChoice);
 void printCompuWord(char *compuWord);
 
 #endif /* Include/header guard. */
